f373092e0880ba3a2d9bd3ac3c8d436f5877c53e
Carlos Cuenca <cuenca.cl@gmail.com>
2022-09-05 21:25:02 -0700

    Initial commit

---
+++ test/clcuenca-dev-cdk-typescript.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ClcuencaDevCdkTypescript from '../lib/clcuenca-dev-cdk-typescript-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/clcuenca-dev-cdk-typescript-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ClcuencaDevCdkTypescript.ClcuencaDevCdkTypescriptStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
